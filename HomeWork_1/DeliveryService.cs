eb1e0b75934de2d59dffbb9ac5acd284eb459814
Karbofos <karbofos22@yandex.ru>
2022-08-23 17:07:04 +0300

    Added Homework_1 files

---
+++ HomeWork_1/DeliveryService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1
+{
+    internal class DeliveryService : Client
+    {
+        public List<object> randomOperator;
+
+        public string ClientsDocuments { get; set; }
+
+        public Cargo CargoAtStorage { get; set; }
+
+        public void ReceiveCargoFromClient(Client client)
+        {
+        }
+        public void issueClientDocuments()
+        {
+
+        }
+        public void PutCargoToStorage()
+        {
+
+        }
+        public void RejectProhibitedCargo()
+        {
+
+        }
+    }
+}
---
+++ HomeWork_1/LogisticsOperator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1
+{
+    internal class LogisticsOperator : DeliveryService
+    {
+        public string OperatorName { get; set; }
+
+        public string Documents { get; set; }
+
+        public Cargo Delivery { get; set; }
+
+        public List<object> drivers;
+
+
+        public void ProcessingDeliveryOrder()
+        {
+
+        }
+        public void ChooseProperDriver()
+        {
+
+        }
+        public void AssignDeliveryOrderToDriver()
+        {
+
+        }
+
+
+
+    }
+}
