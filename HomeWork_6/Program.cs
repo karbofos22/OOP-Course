6a12b27cf518d5fb8f1d4ebbd2418cbecd9ebce1
Karbofos <karbofos22@yandex.ru>
2022-09-09 18:45:56 +0300

    Added HomeWork_6 files

---
+++ HomeWork_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HomeWork_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new(13.2f, 5f, Colors.Yellow, Visibility.False);
+            point.ShowInfo();
+            Console.WriteLine();
+
+            point.MoveByX(-24);
+            point.ChangeColor(Colors.Red);
+            point.GetVisibilityStatus();
+            point.ShowInfo();
+            Console.WriteLine();
+
+            Circle circle = new(24f, 35f, Colors.Blue, Visibility.True, 14);
+            circle.GetCircleArea();
+            circle.ShowInfo();
+            Console.WriteLine();
+
+            circle.MoveByY(5f);
+            circle.ChangeColor(Colors.Green);
+            circle.ShowInfo();
+            Console.WriteLine();
+
+            Rectangle rectangle = new(15f, 25f, 11f, 41f, Colors.Black, Visibility.False);
+            rectangle.GetRectangleArea();
+            rectangle.ChangeColor(Colors.Red);
+            rectangle.ShowInfo();
+        }
+    }
+}
---
+++ HomeWork_6/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6
+{
+    internal class Rectangle : Point
+    {
+        private float SideA { get; set; }
+        private float SideB { get; set; }
+        private float RectangleArea { get; set; }
+
+        /// <summary>
+        ///  sideA - длина стороны А прямоуголника<br/> 
+        ///  sideB - длина стороны B прямоуголника<br/> 
+        ///  positionX - позиция по X координате<br/>  
+        ///  positionY - позиция по Y координате<br/>  
+        ///  setColor - выбрать цвет из перечисления<br/>  
+        ///  visibility - выбрать значение видимости обьекта из перечисления
+        /// </summary>
+        /// <param name="positionX"></param>
+        /// <param name="positionY"></param>
+        /// <param name="setColor"></param>
+        /// <param name="visibility"></param>
+        /// 
+        public Rectangle(float sideA, float sideB, float positionX, float positionY, Colors setColor, Visibility visibility) : base(positionX, positionY, setColor, visibility)
+        {
+            SideA = sideA;
+            SideB = sideB;
+        }
+        public float GetRectangleArea()
+        {
+           return RectangleArea = SideA * SideB;
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Длина стороны А: {SideA} см\n" +
+                              $"Длина стороны B: {SideB} см\n" +
+                              $"Площадь прямоугольника: {RectangleArea} см2");
+        }
+    }
+}
